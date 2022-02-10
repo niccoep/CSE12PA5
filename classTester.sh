074e6443d51b29573fc2cbb2f6e6c79c44ec0184
nep <49878358+niccoep@users.noreply.github.com>
2022-02-10 13:12:00 -0800

    Create classTester.sh

---
+++ classTester.sh
@@ -0,0 +1,49 @@
+java -jar ../public/bin/simulator.jar --data-structure A --size 10000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 20000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 30000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 40000 --operation add --iteration 1000 --round 5
+
+java -jar ../public/bin/simulator.jar --data-structure B --size 10000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 20000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 30000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 40000 --operation add --iteration 1000 --round 5
+
+java -jar ../public/bin/simulator.jar --data-structure C --size 10000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 20000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 30000 --operation add --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 40000 --operation add --iteration 1000 --round 5
+
+echo "------------------------------------------"
+
+java -jar ../public/bin/simulator.jar --data-structure A --size 10000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 20000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 30000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 40000 --operation find --iteration 1000 --round 5
+
+java -jar ../public/bin/simulator.jar --data-structure B --size 10000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 20000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 30000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 40000 --operation find --iteration 1000 --round 5
+
+java -jar ../public/bin/simulator.jar --data-structure C --size 10000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 20000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 30000 --operation find --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 40000 --operation find --iteration 1000 --round 5
+
+echo "------------------------------------------"
+
+java -jar ../public/bin/simulator.jar --data-structure A --size 10000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 20000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 30000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure A --size 40000 --operation remove --iteration 1000 --round 5
+
+java -jar ../public/bin/simulator.jar --data-structure B --size 10000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 20000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 30000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure B --size 40000 --operation remove --iteration 1000 --round 5
+
+java -jar ../public/bin/simulator.jar --data-structure C --size 10000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 20000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 30000 --operation remove --iteration 1000 --round 5
+java -jar ../public/bin/simulator.jar --data-structure C --size 40000 --operation remove --iteration 1000 --round 5
+
